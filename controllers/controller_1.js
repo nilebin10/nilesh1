52ebda81c19c4ddd7f1affbf73f740a55d2a7af5
nilebin10 <nielbin@live.com>
2016-10-16 01:10:22 +0530

    "This is updated controller file"

--- controllers/controller_1.js
+++ controllers/controller_1.js
@@ -6,10 +6,23 @@
 
   function mainControllerFn(){
     this.name = 'Default';
+    this.friendz = [];
     this.getName = getNaame;
+    this.addFriends = addFriends;
+    this.respectBhai = IterateOverFrineds;
 
     function getName(){
       return this.getName;
+    };
+
+    function addFriends(friendName){
+      this.friendz.push(friendName);
+    };
+
+    function IterateOverFrineds(){
+      var newArray = this.friendz.forEach(function(item, index, array){
+          item = item + 'Bhai';
+      });
     }
   }
 })()
